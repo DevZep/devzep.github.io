b63738c7c0bbe8d52bf75ee139e099e69bd70115
Seyha-Eng <seyha.eng09@gmail.com>
2020-10-26 10:12:17 +0700

    merge branch

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,15 +1,30 @@
 // prefer default export if available
-const preferDefault = m => (m && m.default) || m
+const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-header-js": () => import("./../../../src/pages/header.js" /* webpackChunkName: "component---src-pages-header-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("./../../../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-pages-portfolio-js": () => import("./../../../src/pages/portfolio.js" /* webpackChunkName: "component---src-pages-portfolio-js" */),
-  "component---src-pages-service-js": () => import("./../../../src/pages/service.js" /* webpackChunkName: "component---src-pages-service-js" */),
-  "component---src-pages-using-typescript-tsx": () => import("./../../../src/pages/using-typescript.tsx" /* webpackChunkName: "component---src-pages-using-typescript-tsx" */)
+  "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/Users/seyha/Documents/devzep.github.io/src/templates/blog-post.js"),
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/seyha/Documents/devzep.github.io/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/seyha/Documents/devzep.github.io/src/pages/404.js"),
+  "component---src-pages-contact-js": require("gatsby-module-loader?name=component---src-pages-contact-js!/Users/seyha/Documents/devzep.github.io/src/pages/contact.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/seyha/Documents/devzep.github.io/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/seyha/Documents/devzep.github.io/src/pages/page-2.js"),
+  "component---src-pages-portfolio-js": require("gatsby-module-loader?name=component---src-pages-portfolio-js!/Users/seyha/Documents/devzep.github.io/src/pages/portfolio.js")
 }
 
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/seyha/Documents/devzep.github.io/.cache/json/layout-index.json"),
+  "post-one.json": require("gatsby-module-loader?name=path---post-one!/Users/seyha/Documents/devzep.github.io/.cache/json/post-one.json"),
+  "post-two.json": require("gatsby-module-loader?name=path---post-two!/Users/seyha/Documents/devzep.github.io/.cache/json/post-two.json"),
+  "post-three.json": require("gatsby-module-loader?name=path---post-three!/Users/seyha/Documents/devzep.github.io/.cache/json/post-three.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/seyha/Documents/devzep.github.io/.cache/json/dev-404-page.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/seyha/Documents/devzep.github.io/.cache/json/404.json"),
+  "contact.json": require("gatsby-module-loader?name=path---contact!/Users/seyha/Documents/devzep.github.io/.cache/json/contact.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/seyha/Documents/devzep.github.io/.cache/json/index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/seyha/Documents/devzep.github.io/.cache/json/page-2.json"),
+  "portfolio.json": require("gatsby-module-loader?name=path---portfolio!/Users/seyha/Documents/devzep.github.io/.cache/json/portfolio.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/seyha/Documents/devzep.github.io/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/seyha/Documents/devzep.github.io/.cache/layouts/index.js")
+}
